e350177b0d0a62c032e622027a5c6f60f714109c
NicolasTsc <nicolas.tschirr@gmail.com>
2020-11-09 18:52:55 +0100

    refactor to atomic tests

--- news-list/src/components/NewsItem/NewsItem.spec.js
+++ news-list/src/components/NewsItem/NewsItem.spec.js
@@ -3,31 +3,23 @@ import NewsItem from '@/components/NewsItem/NewsItem.vue'
 
 describe('NewsItem.vue', () => {
   describe(':buttons', () => {
-    describe('click remove', () => {
-      const newsItem = mount(NewsItem, {
+    let newsItem
+    beforeEach(() => {
+      newsItem = mount(NewsItem, {
         propsData: {
           item: { id: 20, title: 'Item 1', votes: 0 }
         }
       })
-
+    })
+    describe('click remove', () => {
       it('emits removeitem event', async () => {
         const removeButton = newsItem.find('button.remove-button')
-        removeButton.trigger('click')
-        await newsItem.vm.$nextTick()
-
-        expect(newsItem.emitted().removeitem).toBeTruthy()
-        expect(newsItem.emitted().removeitem.length).toBe(1)
-        expect(newsItem.emitted().removeitem[0]).toEqual([20])
+        await removeButton.trigger('click')
+        expect(newsItem.emitted()).toEqual({ removeitem: [[20]] })
       })
     })
 
     describe('click upvote', () => {
-      const newsItem = mount(NewsItem, {
-        propsData: {
-          item: { id: 20, title: 'Item 1', votes: 0 }
-        }
-      })
-
       it('increases votes by one', async () => {
         const upvoteButton = newsItem.find('button.upvote-button')
         upvoteButton.trigger('click')
@@ -38,12 +30,6 @@ describe('NewsItem.vue', () => {
     })
 
     describe('click downvote', () => {
-      const newsItem = mount(NewsItem, {
-        propsData: {
-          item: { id: 20, title: 'Item 1', votes: 0 }
-        }
-      })
-
       it('decreases votes by one', async () => {
         const upvoteButton = newsItem.find('button.downvote-button')
         upvoteButton.trigger('click')
